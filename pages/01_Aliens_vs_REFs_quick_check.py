284e55114671404c3c8a9fbf2c7d2cd8335e4e86
Mark Winfield <54711106+MarkOWinfield@users.noreply.github.com>
2023-03-22 22:37:49 +0000

    Update 01_Aliens_vs_REFs_quick_check.py

--- pages/01_Aliens_vs_REFs_quick_check.py
+++ pages/01_Aliens_vs_REFs_quick_check.py
@@ -12,6 +12,12 @@ st.set_page_config(layout="wide")
 
 st.markdown(""" <style> .font1 {
     font-size: 45px; font-family: 'Copper Black'; color: #FF9633}
+    }
+    </style> """, unsafe_allow_html=True)
+
+st.markdown(""" <style> .font2 {
+    font-size: 30px; font-family: 'Copper Black'; color: #FF9633}
+    }
     </style> """, unsafe_allow_html=True)
     
  
