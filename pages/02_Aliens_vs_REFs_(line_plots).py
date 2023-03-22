9d1f7e07e209bbef172e5158fe55208cec440317
Mark Winfield <54711106+MarkOWinfield@users.noreply.github.com>
2023-03-22 22:31:54 +0000

    Update 02_Aliens_vs_REFs_(line_plots).py

--- pages/02_Aliens_vs_REFs_(line_plots).py
+++ pages/02_Aliens_vs_REFs_(line_plots).py
@@ -12,15 +12,13 @@ import altair as alt
 st.set_page_config(layout="wide")
 
 st.markdown(""" <style> .font1 {
-    font-size: 45px; font-family: 'Copper Black'; color: #FF9633},
-    .font2 {
-    font-size: 30px; font-family: 'Copper Black'; color: #FF9633}
-        }
+    font-size: 45px; font-family: 'Copper Black'; color: #FF9633}
+    }
     </style> """, unsafe_allow_html=True)
 
 st.markdown(""" <style> .font2 {
     font-size: 30px; font-family: 'Copper Black'; color: #FF9633}
-        }
+    }
     </style> """, unsafe_allow_html=True)
 
 
