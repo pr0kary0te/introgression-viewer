d4c300837836694eee9e3fd3514430902ea75020
Mark Winfield <54711106+MarkOWinfield@users.noreply.github.com>
2023-04-25 14:19:40 +0100

    Update 03_Elites_vs_REFs.py

--- pages/03_Elites_vs_REFs.py
+++ pages/03_Elites_vs_REFs.py
@@ -10,23 +10,5 @@ import altair as alt
 
 st.set_page_config(layout="wide")
 
-st.markdown(""" <style> .font1 {
-    font-size: 45px; font-family: 'Copper Black'; color: #FF9633}
-    </style> """, unsafe_allow_html=True)
-
-st.markdown(""" <style> .font2 {
-    font-size: 30px; font-family: 'Copper Black'; color: #FF9633}
-    </style> """, unsafe_allow_html=True)
-    
-st.markdown(""" <style> .font2b {
-    font-size: 30px; font-family: 'Copper Black'; color: #1F77B4}
-    </style> """, unsafe_allow_html=True)    
-
-st.markdown(""" <style> .font2o {
-    font-size: 30px; font-family: 'Copper Black'; color: #FF7F0E}
-    </style> """, unsafe_allow_html=True)
-    
-st.markdown(""" <style> .red { color: #FF0000} </style> """, unsafe_allow_html=True)
-
 
 st.write('Hello, this page works')
