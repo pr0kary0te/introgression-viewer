bb882d0c704d8e7ce3166cf1424b1e8c1789ad25
Mark Winfield <54711106+MarkOWinfield@users.noreply.github.com>
2023-11-01 17:33:48 +0000

    Update Gift.py

--- pages/Gift.py
+++ pages/Gift.py
@@ -226,7 +226,7 @@ dfBins = pd.DataFrame.from_dict(binDict)
 with st.expander('Click to view bin boundaries', expanded = False):
     st.table(binDict)
 
-st.header(f'Number of Genes in {number} Bins Spanning Chromosome {chrm}')
+st.header('Number of Genes in ' + number + ' Bins Spanning Chromosome ' + chrm)
 
 st.markdown('''
                 Hover over the bars of the plot to see the number of genes.
