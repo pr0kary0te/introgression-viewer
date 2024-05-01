0c170ec96d78dbb043e4e2c992bae92816de87d5
Mark Winfield <54711106+MarkOWinfield@users.noreply.github.com>
2024-05-01 15:27:16 +0100

    Update 03_Elites_vs_REFs_NEW.py
    Added a a form (line 52) and a submit button (line 112) to the form so that one can change both reference and chromosome without the script running as soon as only one of them has changed.

--- pages/03_Elites_vs_REFs_NEW.py
+++ pages/03_Elites_vs_REFs_NEW.py
@@ -48,65 +48,68 @@ def change_index(df):
 ###############################################################################
 
 with st.sidebar:
+
+    with st.form('my_form', clear_on_submit=False):
     
-    refGenome = st.selectbox(
-        label='Select Reference Genome:',
-        options=('Arina', 'Chinese Spring', 'Jagger',
-                 'Julius', 'Lancer', 'Landmark', 'Mace',
-                 'Norin61', 'Spelt', 'Stanley', 'Mattis'),
-        index=0,
-        key='ref')
+        refGenome = st.selectbox(
+            label='Select Reference Genome:',
+            options=('Arina', 'Chinese Spring', 'Jagger',
+                     'Julius', 'Lancer', 'Landmark', 'Mace',
+                     'Norin61', 'Spelt', 'Stanley', 'Mattis'),
+            index=0,
+            key='ref')
     
-    chrm = st.selectbox(
-        label='Select chromosome:',
-        options=('1A', '2A', '3A', '4A', '5A', '6A', '7A',
-                 '1B', '2B', '3B', '4B', '5B', '6B', '7B',
-                '1D', '2D', '3D', '4D', '5D', '6D', '7D'),
-        index=0,
-        key='chrm')  
+        chrm = st.selectbox(
+            label='Select chromosome:',
+            options=('1A', '2A', '3A', '4A', '5A', '6A', '7A',
+                     '1B', '2B', '3B', '4B', '5B', '6B', '7B',
+                    '1D', '2D', '3D', '4D', '5D', '6D', '7D'),
+            index=0,
+            key='chrm')  
     
 
-    if int(chrm[0]) <= 4:
+        if int(chrm[0]) <= 4:
 
-        refFiles = {'Arina': ['https://www.cerealsdb.uk.net/ibspy_data/arina1_4.csv', '_WhAri'],
-              'Chinese Spring': ['https://www.cerealsdb.uk.net/ibspy_data/chinese1_4.csv', ''],
-              'Jagger': ['https://www.cerealsdb.uk.net/ibspy_data/jagger1_4.csv', '_WhJag'],
-              'Julius': ['https://www.cerealsdb.uk.net/ibspy_data/julius1_4.csv', '_Whjul'],
-              'Lancer': ['https://www.cerealsdb.uk.net/ibspy_data/lancer1_4.csv', '_Whlan'], 
-              'Landmark': ['https://www.cerealsdb.uk.net/ibspy_data/landmark1_4.csv', '_WhLan'],
-              'Mace': ['https://www.cerealsdb.uk.net/ibspy_data/mace1_4.csv', '_Whmac'],
-              'Norin61': ['https://www.cerealsdb.uk.net/ibspy_data/norin1_4.csv', '_WhNor'],
-              'Spelt': ['https://www.cerealsdb.uk.net/ibspy_data/spelta1_4.csv', '_Whspe'],
-              'Stanley': ['https://www.cerealsdb.uk.net/ibspy_data/stanley1_4.csv', '_WhSta'],
-              'Mattis': ['https://www.cerealsdb.uk.net/ibspy_data/mattis1_4.csv', '_WhSYM']
-              }
+            refFiles = {'Arina': ['https://www.cerealsdb.uk.net/ibspy_data/arina1_4.csv', '_WhAri'],
+                  'Chinese Spring': ['https://www.cerealsdb.uk.net/ibspy_data/chinese1_4.csv', ''],
+                  'Jagger': ['https://www.cerealsdb.uk.net/ibspy_data/jagger1_4.csv', '_WhJag'],
+                  'Julius': ['https://www.cerealsdb.uk.net/ibspy_data/julius1_4.csv', '_Whjul'],
+                  'Lancer': ['https://www.cerealsdb.uk.net/ibspy_data/lancer1_4.csv', '_Whlan'], 
+                  'Landmark': ['https://www.cerealsdb.uk.net/ibspy_data/landmark1_4.csv', '_WhLan'],
+                  'Mace': ['https://www.cerealsdb.uk.net/ibspy_data/mace1_4.csv', '_Whmac'],
+                  'Norin61': ['https://www.cerealsdb.uk.net/ibspy_data/norin1_4.csv', '_WhNor'],
+                  'Spelt': ['https://www.cerealsdb.uk.net/ibspy_data/spelta1_4.csv', '_Whspe'],
+                  'Stanley': ['https://www.cerealsdb.uk.net/ibspy_data/stanley1_4.csv', '_WhSta'],
+                  'Mattis': ['https://www.cerealsdb.uk.net/ibspy_data/mattis1_4.csv', '_WhSYM']
+                  }
     
-    else:
+        else:
 
-        refFiles = {'Arina': ['https://www.cerealsdb.uk.net/ibspy_data/arina5_7.csv', '_WhAri'],
-              'Chinese Spring': ['https://www.cerealsdb.uk.net/ibspy_data/chinese5_7.csv', ''],
-              'Jagger': ['https://www.cerealsdb.uk.net/ibspy_data/jagger5_7.csv', '_WhJag'],
-              'Julius': ['https://www.cerealsdb.uk.net/ibspy_data/julius5_7.csv', '_Whjul'],
-              'Lancer': ['https://www.cerealsdb.uk.net/ibspy_data/lancer5_7.csv', '_Whlan'], 
-              'Landmark': ['https://www.cerealsdb.uk.net/ibspy_data/landmark5_7.csv', '_WhLan'],
-              'Mace': ['https://www.cerealsdb.uk.net/ibspy_data/mace5_7.csv', '_Whmac'],
-              'Norin61': ['https://www.cerealsdb.uk.net/ibspy_data/norin5_7.csv', '_WhNor'],
-              'Spelt': ['https://www.cerealsdb.uk.net/ibspy_data/spelta5_7.csv', '_Whspe'],
-              'Stanley': ['https://www.cerealsdb.uk.net/ibspy_data/stanley5_7.csv', '_WhSta'],
-              'Mattis': ['https://www.cerealsdb.uk.net/ibspy_data/mattis5_7.csv', '_WhSYM']
-              }
+            refFiles = {'Arina': ['https://www.cerealsdb.uk.net/ibspy_data/arina5_7.csv', '_WhAri'],
+                  'Chinese Spring': ['https://www.cerealsdb.uk.net/ibspy_data/chinese5_7.csv', ''],
+                  'Jagger': ['https://www.cerealsdb.uk.net/ibspy_data/jagger5_7.csv', '_WhJag'],
+                  'Julius': ['https://www.cerealsdb.uk.net/ibspy_data/julius5_7.csv', '_Whjul'],
+                  'Lancer': ['https://www.cerealsdb.uk.net/ibspy_data/lancer5_7.csv', '_Whlan'], 
+                  'Landmark': ['https://www.cerealsdb.uk.net/ibspy_data/landmark5_7.csv', '_WhLan'],
+                  'Mace': ['https://www.cerealsdb.uk.net/ibspy_data/mace5_7.csv', '_Whmac'],
+                  'Norin61': ['https://www.cerealsdb.uk.net/ibspy_data/norin5_7.csv', '_WhNor'],
+                  'Spelt': ['https://www.cerealsdb.uk.net/ibspy_data/spelta5_7.csv', '_Whspe'],
+                  'Stanley': ['https://www.cerealsdb.uk.net/ibspy_data/stanley5_7.csv', '_WhSta'],
+                  'Mattis': ['https://www.cerealsdb.uk.net/ibspy_data/mattis5_7.csv', '_WhSYM']
+                  }
         
-    filePath = refFiles[refGenome][0]    
+        filePath = refFiles[refGenome][0]    
 
-    dfWhole = getData(filePath)
+        dfWhole = getData(filePath)
         
-    dfWhole.rename(columns={'timopheevii10558_nuq.jf': 'timopheevii10558_nuq_jf',
-                       'timopheevii14352_nuq.jf': 'timopheevii14352_nuq_jf',
-                       'timopheevii15832_nuq.jf': 'timopheevii15832_nuq_jf',
-                       'timopheevii22438_nuq.jf': 'timopheevii22438_nuq_jf',
-                       'timopheevii3708_nuq.jf': 'timopheevii3708_nuq_jf'},
-                         inplace=True)
-
+        dfWhole.rename(columns={'timopheevii10558_nuq.jf': 'timopheevii10558_nuq_jf',
+                           'timopheevii14352_nuq.jf': 'timopheevii14352_nuq_jf',
+                           'timopheevii15832_nuq.jf': 'timopheevii15832_nuq_jf',
+                           'timopheevii22438_nuq.jf': 'timopheevii22438_nuq_jf',
+                           'timopheevii3708_nuq.jf': 'timopheevii3708_nuq_jf'},
+                             inplace=True)
+   
+        submit = st.form_submit_button('Submit')   
    
 st.markdown('<h1 class="font1">Introgression in Elites varieties</h1>', unsafe_allow_html=True)    
 st.markdown("""
